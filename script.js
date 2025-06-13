ca54b75aa52d8496e120d820f68a7876347887ef
Shairaaj <iamshairaaj@gmail.com>
2025-06-13 19:18:22 +0530

    Multiplicatin Application

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-const hour= document.getElementById('hour');
-const min = document.getElementById("min");
-const sec = document.getElementById("sec");
-const ampm = document.getElementById("ampm");
+const formEl= document.getElementById("form");
+const scoreEl= document.getElementById("score");
+const queEl= document.getElementById("que");
+const inputEl= document.getElementById("input");
+const resetEl= document.getElementById("reset");
+
+const num1= Math.ceil(Math.random()*10);
+const num2= Math.ceil(Math.random()*10);
 
 
-function updateClock(){
-    let h= new Date().getHours();
-    let m= new Date().getMinutes();
-    let s= new Date().getSeconds();
-    let ampm = "AM";
+queEl.innerHTML=`What is ${num1} multiplied by ${num2} ?`;
+
+const correctAns= num1*num2;
+let score=+localStorage.getItem("score");
+
+formEl.addEventListener("submit",()=>{
+    const userAns= +input.value;
+    console.log(userAns);
     
-    if(h>12){
-        h=h-12;
-        ampm="PM";
+    if(userAns===correctAns){
+        localStorage.setItem("score",score+1);
+        console.log(score);
     }
-
-    h=h<10 ? '0'+h : h;
-    m=m<10 ? '0'+m : m;
-    s=s<10 ? '0'+s : s;
-
-    hour.innerText=h;
-    min.innerText=m;
-    sec.innerText=s;
-    ampm.innerText=ampm;
-    setTimeout(()=>{
-        updateClock(); 
-    },1000);
+    else{
+        localStorage.setItem("score",score-1);
+        console.log(score);
+    }
 }
-
-updateClock();
+)
+resetEl.addEventListener("click",()=>{
+    localStorage.setItem("score",0);
+    scoreEl.innerHTML = `Score: ${+localStorage.getItem("score")}`;
+})
+scoreEl.innerHTML=`Score: ${score}`;
