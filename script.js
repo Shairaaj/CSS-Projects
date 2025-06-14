89a148e158895f8ba65465576ad04388aa30d177
Shairaaj <iamshairaaj@gmail.com>
2025-06-14 08:08:22 +0530

    Button with ripple effects

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
-const formEl= document.getElementById("form");
-const scoreEl= document.getElementById("score");
-const queEl= document.getElementById("que");
-const inputEl= document.getElementById("input");
-const resetEl= document.getElementById("reset");
+const btnEl= document.querySelector(".btn");
 
-const num1= Math.ceil(Math.random()*10);
-const num2= Math.ceil(Math.random()*10);
-
-
-queEl.innerHTML=`What is ${num1} multiplied by ${num2} ?`;
-
-const correctAns= num1*num2;
-let score=+localStorage.getItem("score");
-
-formEl.addEventListener("submit",()=>{
-    const userAns= +input.value;
-    console.log(userAns);
-    
-    if(userAns===correctAns){
-        localStorage.setItem("score",score+1);
-        console.log(score);
-    }
-    else{
-        localStorage.setItem("score",score-1);
-        console.log(score);
-    }
-}
-)
-resetEl.addEventListener("click",()=>{
-    localStorage.setItem("score",0);
-    scoreEl.innerHTML = `Score: ${+localStorage.getItem("score")}`;
+btnEl.addEventListener("mouseover",(event)=>{
+    const x=event.pageX-btnEl.offsetLeft;
+    const y= event.pageY-btnEl.offsetTop;
+    btnEl.style.setProperty("--x", x+"px");
+    btnEl.style.setProperty("--y", y+"px");
 })
-scoreEl.innerHTML=`Score: ${score}`;
